d0f7b47b76db083e6510e98a0c8920f8ee59adbf
balamurugan <sbalamurugan2911@gmail.com>
2023-11-22 21:05:32 +0530

    map

---
+++ core_java/src/collection/Map_demo.java
@@ -0,0 +1,21 @@
+package collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Map_demo {
+
+	public static void main(String[] args) {
+		HashMap m=new HashMap();
+		
+		m.put(1, 23);
+		m.put(2, 28);
+		m.put(3, 33);
+		m.put(4, 24);
+		m.put(5, 15);
+		
+		System.out.println(m);
+
+	}
+
+}
